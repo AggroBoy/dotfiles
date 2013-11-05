4092b0f3c79d3aa856be4f327800ab0755af35db
Will Goring <A>
2013-11-05 19:42:59 +0000

    made path detection (more) platform independant

--- install.sh
+++ install.sh
@@ -1,11 +1,17 @@
 #!env zsh
 
-# Where are the dotfiles going? ($HOME of course)
-TARGETDIR=$HOME
-
-# Get the dotfiles directory
-SOURCEDIR=$(realpath --relative-to=$TARGETDIR $(dirname $0))
-
+realpath() {
+  OURPWD=$PWD
+  cd "$(dirname "$1")"
+  LINK=$(readlink "$(basename "$1")")
+  while [ "$LINK" ]; do
+    cd "$(dirname "$LINK")"
+    LINK=$(readlink "$(basename "$1")")
+  done
+  REALPATH="$PWD/$(basename "$1")"
+  cd "$OURPWD"
+  echo "$REALPATH"
+}
 
 # A function to link a file from this directory as a dotfile in the user's $HOME
 # Assumes that the file should have the same name with a '.' prepended.
@@ -21,6 +27,15 @@ makelink () {
     ln -s $SOURCE $TARGET
 }
 
+
+# Where are the dotfiles going? ($HOME of course)
+TARGETDIR=$HOME
+
+# Get the dotfiles directory
+SOURCEDIR=$(realpath $(dirname $0))
+echo $SOURCEDIR
+SOURCEDIR=$(echo $SOURCEDIR | sed -e "s|/.$||")
+SOURCEDIR=$(echo $SOURCEDIR | sed -e "s|^$TARGETDIR/||")
 
 # Create a backups directory to put old files in
 # my vim config uses this too, so it's good to make sure it's there.
